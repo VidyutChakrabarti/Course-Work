98143b176da6fbe22d26604502c59d20120e8f23
VidyutChakrabarti <chakrabartivr@rknec.edu>
2025-01-31 17:08:36 +0530

    adapter code snippet added

---
+++ DP example snippets/Adapter DP code/src/mp4Player.java
@@ -0,0 +1,9 @@
+    public class mp4Player implements advancedMediaPlayer{
+    public void playVlc(String audioType,String fileName){
+
+}
+    public void playMp4( String audioType,String fileName){
+//audioPlayer can play VLC files 
+           System.out.println("I can play mp4 files "+fileName);
+}
+}
