9ae84d55b65a569e452bb423e5bc30e217f6605b
VidyutChakrabarti <chakrabartivr@rknec.edu>
2025-06-15 08:19:46 +0530

    composite-dp code snippet added

---
+++ DP example snippets/CompositeDP code/src/Employee.java
@@ -0,0 +1,6 @@
+public interface Employee                //called as Component
+{
+    public void showEmployeeDetails();
+    public void addEmployee(Employee emp);
+    public void removeEmployee(Employee emp);
+}
